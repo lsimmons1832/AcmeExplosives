29a46f9829241b3233fd44a03dc43df38007102b
Latasha Simmons <lsimmons1832@gmail.com>
2017-04-15 13:59:18 -0500

    working on writing to the DOM

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,54 +1,136 @@
-$(document).ready(function() {  
-	products = [];
-console.log(products);
+$(document).ready(function() {
+  var products = [];
+  console.log(products);
+
   function writeDOM() {
-      var domString = "";
-      for (var i = 0; i < products.length; i++) {
-          domString += `<h1>${products[i].name}</h1>`;
-      }
-      $(".container").append(domString);
+      window.addEventListener("click", function() {
+          var fireworks = $('.fireworks');
+          if (event.target.id === 'fireworks') {
+              alert("I selected fireworks");
+              $.each(products, function(index, value){
+              	console.log(index);
+              	console.log(value);
+              	if(value.name === "Fireworks" && value.id === value.category){
+              		alert("Hello there!");
+              	}
+              })
+
+          } else {
+              alert("I selected demolition");
+          }
+      });
   }
 
 
-var categoriesJSON = function () {
-	return new Promise(function(resolve, reject){
-		$.ajax("./db/categories.json").done(function(data1){
-			resolve(data1.categories);
-		}).fail(function(error1){
-			reject(error1);
-		})
-	})
-};
+  var categoriesJSON = function() {
+      return new Promise(function(resolve, reject) {
+          $.ajax("./db/categories.json").done(function(data1) {
+              resolve(data1.categories);
+          }).fail(function(error1) {
+              reject(error1);
+          })
+      })
+  };
 
-var productsJSON = function () {
-	return new Promise(function(resolve, reject){
-		$.ajax("./db/products.json").done(function(data2){
-			resolve(data2.products);
-		}).fail(function(error2){
-			reject(error2);
-		})
-	})
-};
+  var typesJSON = function() {
+      return new Promise(function(resolve, reject) {
+          $.ajax("./db/types.json").done(function(data2) {
+              resolve(data2.types);
+          }).fail(function(error2) {
+              reject(error2);
+          })
+      })
+  };
 
-var typesJSON = function () {
-	return new Promise(function(resolve, reject){
-		$.ajax("./db/types.json").done(function(data3){
-			resolve(data3.types);
-		}).fail(function(error3){
-			reject(error3);
-		})
-	})
-};
+  var productsJSON = function() {
+      return new Promise(function(resolve, reject) {
+          $.ajax("./db/products.json").done(function(data3) {
+              resolve(data3.products);
+          }).fail(function(error3) {
+              reject(error3);
+          })
+      })
+  };
 
-Promise.all([categoriesJSON(), productsJSON(), typesJSON()])
-	.then(function(result){
-		console.log("result", result);
-		result.forEach(function(ajaxCalls){
-			ajaxCalls.forEach(function(product){
-			products.push(product);
-		})
-	})
-	writeDOM();
-})
 
-});
+
+	categoriesJSON().then(function(data){
+		console.log(data);
+		data.forEach(function(category){
+			products.push(category);
+		})
+			return typesJSON();
+		}).then(function(data2){
+			console.log(data2);
+			data2.forEach(function(type){
+				products.push(type);
+			})
+			return productsJSON();
+		}).then(function(data3){
+			console.log(data3);
+			data3.forEach(function(product){
+				products.push(product);
+			})
+			writeDOM();
+		});
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	});
